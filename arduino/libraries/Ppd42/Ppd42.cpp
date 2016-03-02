51969c825fc9dc0a1f1dd9a2dd4e5faa21aafb18
lebeno <github@kks36.be>
2016-03-02 18:54:39 +0100

    corrected millis to micros

--- arduino/libraries/Ppd42/Ppd42.cpp
+++ arduino/libraries/Ppd42/Ppd42.cpp
@@ -23,20 +23,15 @@ Ppd42::Ppd42(){
 
 void Ppd42::begin() {
 	// initialization for the dust sensor
-	readTimePM10 = millis(); //Fetch the current time
-	readTimePM25 = millis(); //Fetch the current time
+	readTimePM10 = micros(); //Fetch the current time
+	readTimePM25 = micros(); //Fetch the current time
 	debug="";
 
-	//pinMode(PPD_PM25_PIN, FUNCTION_3); //Set TX PIN to GPIO
-	//pinMode(PPD_PM10_PIN, FUNCTION_3); //Set RX PIN to GPIO
-	//pinMode(PPD_PM25_PIN, INPUT_PULLUP); //Listen at the designated PIN
 	pinMode(PPD_PM10_PIN, INPUT);
 	pinMode(PPD_PM25_PIN, INPUT);
 	activated = true;
 	attachInterrupt(PPD_PM10_PIN, handleInterruptPM10, CHANGE); // Attaching interrupt to PIN
 	attachInterrupt(PPD_PM25_PIN, handleInterruptPM25, CHANGE); // Attaching interrupt to PIN
-	//pinMode(PPD_PM10_PIN, INPUT_PULLUP); //Listen at the designated PIN
-	//attachInterrupt(PPD_PM10_PIN, intrLOPM10, CHANGE); // Attaching interrupt to PIN
 }
 
 void Ppd42::end() {
@@ -47,7 +42,7 @@ void Ppd42::read() {
 
 double Ppd42::readPM10() {
 	if (activated) {
-		unsigned long currentTime=millis(); 
+		unsigned long currentTime=micros(); 
 		unsigned long sampledTime = currentTime - readTimePM10;
 		unsigned long triggeredTotalTime = triggeredTotalTimePM10;
 		readTimePM10 = currentTime;
@@ -60,7 +55,7 @@ double Ppd42::readPM10() {
 
 double Ppd42::readPM25() {
 	if (activated) {
-		unsigned long currentTime=millis(); 
+		unsigned long currentTime=micros(); 
 		unsigned long sampledTime = currentTime - readTimePM25;
 		unsigned long triggeredTotalTime = triggeredTotalTimePM25;
 		readTimePM25 = currentTime;
@@ -93,6 +88,8 @@ void Ppd42::handleInterruptPM10() {
 		if (!triggerStartedPM10) {
 			triggerStartedPM10 = true;
 			triggerStartTimePM10 = micros();
+		}
+		else {
 		}
 	else	// not LOW, thus end of the trigger
 		if (triggerStartedPM10) {
