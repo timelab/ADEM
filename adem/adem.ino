2203c57ba8c4bb4f5c7ea000c249bab55d7d956e
Dag Wieers <dag@wieers.com>
2016-04-11 01:56:48 +0200

    Added Ppd42 sensor and bootup messages

--- adem/adem.ino
+++ adem/adem.ino
@@ -1,31 +1,22 @@
 #include <Adafruit_NeoPixel.h>
 #include <Ppd42.h>
-#include <Wire.h>
-#include <Sensor.h>
-#include <ArduinoJson.h>
 #include <TickerSchedlr.h>
 #include <BMP085Sensor.h>
 #include <HTU21DFSensor.h>
-#include <TinyGPS++.h>
-#include <SoftwareSerial.h>
+//#include <GPSSensor.h>
 
 #define NEOPIXEL_PIN 0
 #define I2C_SDA_PIN 2
 #define I2C_SCL_PIN 14
-#define GPS_RX_PIN 4                    // to GPS module RX pin. Does not work on GPIO 16 (XPD).
-#define GPS_TX_PIN SW_SERIAL_UNUSED_PIN // we send no data to the GPS module
 #define SERIAL_RX_PIN 8
 #define SERIAL_TX_PIN 7
 
-#define GPS_BAUD 9600
 #define SERIAL_BAUD 74880
 
 BMP085Sensor druksensor;
 HTU21DFSensor vochtsensor;
 Adafruit_NeoPixel neopixel = Adafruit_NeoPixel(1, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
 Ppd42 Ppd42Sensor;
-SoftwareSerial gps_Serial(GPS_RX_PIN, GPS_TX_PIN);
-TinyGPSPlus gps;
 TickerSchedlr *schedule = 0; //TickerSchedlr::Instance(200);
 
 int count = 0;
@@ -37,7 +28,6 @@ TickerTask * T3 = NULL;
 int pin1 = 0;
 int pin2 = 14;
 int pin3 = 13;
-//test test2;
 
 void flip(void *)
 {
@@ -49,6 +39,7 @@ void flip(void *)
   ++count;
   Serial.println(vochtsensor.report());
   Serial.println(druksensor.report());
+  Serial.println(Ppd42Sensor.report());
 }
 
 void flip2(void *ptr)
@@ -77,31 +68,31 @@ void flip3(void *)
 
   vochtsensor.read();
   druksensor.read();
-
+  Ppd42Sensor.read();
 }
 
 void setup() {
   Serial.begin(SERIAL_BAUD);
-  __LOGLN("Boot started.");
-  __LOGLN("Serial communication initialized...");
+  Serial.println("Setup started.");
+  Serial.println("Serial communication initialized...");
   
   neopixel.begin();
-  neopixel.setBrightness(64);
-  neopixel.show(); // Initialize all pixels to 'off'
-  neopixel.setPixelColor(0,255, 0, 255);
-  __LOGLN("NeoPixel initialized...");
+  neopixel.setBrightness(63);
+  neopixel.setPixelColor(0, 63, 0, 63);
+  neopixel.show();
+  Serial.println("NeoPixel initialized...");
 
 //  gps_Serial.begin(GPS_BAUD);  
-//  __LOGLN("GPS initialized...");
+//  Serial.println("GPS initialized...");
 
-//  __LOG("Initializing PPD42...");
-//  Ppd42Sensor.begin();
-//  __LOGLN(" done");
+  Serial.print("Initializing PPD42...");
+  Ppd42Sensor.begin();
+  Serial.println(" done");
 
-  pinMode(14, OUTPUT);
-  digitalWrite(14, LOW);
-  pinMode(13, OUTPUT);
-  digitalWrite(13, LOW);
+  pinMode(pin2, OUTPUT);
+  digitalWrite(pin2, LOW);
+  pinMode(pin3, OUTPUT);
+  digitalWrite(pin3, LOW);
 
   schedule = TickerSchedlr::Instance(200);
   T1 = TickerTask::createPeriodic(&flip, 5000);
@@ -124,15 +115,15 @@ void setup() {
   __LOG("Schedule time "); __LOGLN(schedule->getTickTime());
 
   __LOGLN("schedule added");
+  Serial.println("Tasks created...");
   
   vochtsensor.begin();
-  __LOGLN("Humidity sensor initialized...");
+  Serial.println("Humidity sensor initialized...");
   
   druksensor.begin();
-  __LOGLN("Pressure sensor initialized...");
+  Serial.println("Pressure sensor initialized...");
   
-  __LOGLN("Boot finished.");
-
+  Serial.println("Setup finished.");
 }
 
 // the loop function runs over and over again until power down or reset
