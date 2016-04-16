d4e8a82aa52426b7d8163addf81fe7856ac5a298
Dag Wieers <dag@wieers.com>
2016-04-16 16:38:13 +0200

    Redesign state management, create state and transition functions
    We now have different state functions and transition functions.
    The state management is now done by 2 different case constructions.
    One for handling the state, and one for handling the transitions.

--- adem/adem.ino
+++ adem/adem.ino
@@ -15,8 +15,11 @@
 #define SERIAL_BAUD 74880
 
 // Different states of the program
-enum state_t {START, SLEEP, CONFIG, GPS_TEST, COLLECT, WIFI_TEST, UPLOAD, COLLECT_START, COLLECT_STOP, GPS_START, GPS_STOP, WIFI_START, WIFI_STOP, AP_START, AP_STOP};
+enum state_t { START, SLEEP, CONFIG, GPSTEST, COLLECT, WIFITEST, UPLOAD, RESET };
+char *states[] { "START", "SLEEP", "CONFIG", "GPSTEST", "COLLECT", "WIFITEST", "UPLOAD", "RESET" };
+state_t prev_state = START;
 state_t state = START;
+state_t next_state = SLEEP;
 
 // Objects for all the sensor libraries
 //MPU6050Sensor accelerometer;
@@ -31,6 +34,7 @@ PPD42Sensor particulate;
 const int SCHED_MAX_TASKS = 200;
 TickerSchedlr *schedule = TickerSchedlr::Instance(SCHED_MAX_TASKS);
 
+
 // Tasks
 TickerTask *accelerometer_task = NULL;
 TickerTask *barometer_task = NULL;
@@ -42,8 +46,7 @@ TickerTask *particulate_task = NULL;
 TickerTask *wifiap_task = NULL;
 TickerTask *wificl_task = NULL;
 
-// Task code blocks
-
+// TASKS
 void accelerometer_run(void *) {
   Serial.println(":accelerometer:");
 }
@@ -84,13 +87,9 @@ void particulate_run(void *) {
 //  Serial.println(":wificlient_run:");
 //}
 
-void setup() {
-  Serial.begin(SERIAL_BAUD);
-  Serial.println("Setup started.");
-  Serial.println("Serial communication initialized...");
 
-  led.begin();
-  Serial.println("LED initialized...");
+// STATES
+void start_state() {
   led.setcolor(0, 63, 0, 0); // Red
 
   //accelerator.begin()
@@ -108,12 +107,12 @@ void setup() {
   particulate.begin();
   Serial.println("Particulate sensor initialized...");
 
-  accelerometer_task = TickerTask::createPeriodic(&accelerometer_run, 500);
-  accelerometer_task->name = "accelerometer";
+  //accelerometer_task = TickerTask::createPeriodic(&accelerometer_run, 500);
+  //accelerometer_task->name = "accelerometer";
   barometer_task = TickerTask::createPeriodic(&barometer_run, 60000);
   barometer_task->name = "barometer";
-  buzzer_task = TickerTask::createPeriodic(&buzzer_run, 100);
-  buzzer_task->name = "buzzer";
+  //buzzer_task = TickerTask::createPeriodic(&buzzer_run, 100);
+  //buzzer_task->name = "buzzer";
   gps_task = TickerTask::createPeriodic(&gps_run, 1000);
   gps_task->name = "gps";
   humidity_task = TickerTask::createPeriodic(&humidity_run, 60000);
@@ -128,155 +127,247 @@ void setup() {
   //wificlient_task->name = "wificlient";
   Serial.println("All tasks created...");
 
-  Serial.print("Task accelerometer next time is "); Serial.println(accelerometer_task->getNextTime());
-  Serial.print("Task accelerometer period is "); Serial.println(accelerometer_task->interval);
-  Serial.print("Task accelerometer type is "); Serial.println(accelerometer_task->tasktype);
-  Serial.print("Schedule time "); Serial.println(schedule->getTickTime());
-
-  Serial.println("Setup finished.");
-
-  state = SLEEP;
-  led.setcolor(0, 0, 0, 0); // Black
+  next_state = SLEEP;
 }
 
-// the loop function runs over and over again until power down or reset
-void loop() {
-  
-  if (state == SLEEP) {
+void sleep_state() {
 
-    // if (accelerometer.moving) {
-    if (true) {
-
-        state = GPS_START;
-        //gps.begin();
-        // Activate gps task
-
-        state = GPS_TEST;
-        led.setcolor(0, 63, 31, 0); // Orange
-
-    } else {
-
-      // if (! buffer.empty) {
-      if (false) {
-
-        state = WIFI_START;
-        // wificlient.begin();
-        // Activate wificlient task
-
-        state = WIFI_TEST;
-        led.setcolor(0, 63, 0, 63); // Purple
-
-      //} else if (accelerometer.shaking) {
-      } else if (false) {
-
-        state = AP_START;
-        // wifiap.begin();
-        // Activate wifiap task
-
-        state = CONFIG;
-        led.setcolor(0, 0, 63, 63); // Yellow
-
-      }
-
+  // if (accelerometer.moving) {
+  if (true) {
+    next_state = GPSTEST;
+  } else {
+    // if (! buffer.empty) {
+    if (false) {
+      next_state = WIFITEST;
+    //} else if (accelerometer.shaking) {
+    } else if (false) {
+      next_state = CONFIG;
     }
+  }
+}
 
-  } else if (state == CONFIG) {
+void config_state() {
 
     //if (finished || timeout || canceled) {
     if (true) {
-
-      state = AP_STOP;
-      // Suspend wifiap task
-      //wifiap.end();
-
-      state = SLEEP;
-      led.setcolor(0, 0, 0, 0); // Black
-
+      next_state = SLEEP;
     }
+}
 
-  } else if (state = GPS_TEST) {
+void gpstest_state() {
 
     //if (accelerometer.moving) {
     if (true) {
-
       //if (gps.ready) {
       if (true) {
-
-        state = COLLECT_START;
-        barometer.begin();
-        humidity.begin();
-        particulate.begin();
-        // Activate barometer task
-        // Activate humidity task
-        // Activate particulate task
-
-        state = COLLECT;
-        led.setcolor(0, 0, 63, 0); // Green
-
+        next_state = COLLECT;
       }
-
     } else {
-
-      state = GPS_STOP;
-      // Suspend barometer task
-      // Suspend humidity task
-      // Suspend particulate task
-      // Suspend gps task
-      barometer.end();
-      humidity.end();
-      particulate.end();
-      //gps.end();
-
-      state = SLEEP;
-      led.setcolor(0, 0, 0, 0); // Black
-
+      next_state = SLEEP;
     }
+}
 
-  } else if (state == COLLECT) {
+void collect_state() {
 
-    // sensor tasks should be reporting on their own
-    //Serial.println("Collecting...");
+  // sensor tasks should be reporting on their own
+  Serial.println("Collecting...");
 
-    //if (! accelerometer.moving || buffer.empty || wifi.timeout )) {
-    if (false) {
+  //if (! accelerometer.moving || buffer.empty || wifi.timeout )) {
+  if (false) {
+    next_state = GPSTEST;
+  }
+}
 
-      state = GPS_TEST;
-      led.setcolor(0, 63, 31, 0); // Orange
- 
-    }
-
-  } else if (state == WIFI_TEST) {
+void wifitest_state() {
 
     //if (wificlient.fix) {
     if (false) {
-
       state = UPLOAD;
-      led.setcolor(0, 0, 0, 63); // Blue
-
     }
 
     //if (accelerometer.moving || buffer.empty || wificlient.timeout) {
     if (false) {
-      state = WIFI_STOP;
-      // Suspend wificlient task
-      // wificlient.end();
+      next_state = SLEEP;
+    }
+}
 
-      state = SLEEP;
-      led.setcolor(0, 0, 0, 0); // Black
+void upload_state() {
+
+  // Upload action finishes successfully or times out
+  // Create JSON of X records
+  // Send to server
+  // Empty datastore
+
+  next_state = WIFITEST;
+}
+
+
+// TRANSITIONS
+void start_to_sleep() {
+  Serial.println("start_to_sleep begins");
+  led.setcolor(0, 0, 0, 0); // Black
+  Serial.println("start_to_sleep ends");
+}
+
+void sleep_to_config() {
+
+  led.setcolor(0, 0, 63, 63); // Yellow
+  //wifiap.begin();
+  // Resume wifiap task
+}
+
+void sleep_to_gpstest() {
+
+  led.setcolor(0, 63, 31, 0); // Orange
+  //gps.begin();
+  // Resume gps task
+}
+
+void sleep_to_wifitest() {
+
+  led.setcolor(0, 63, 0, 63); // Purple
+  //wificlient.begin();
+  // Resume wificlient task
+}
+
+void config_to_sleep() {
+
+  led.setcolor(0, 0, 0, 0); // Black
+  // Suspend wifiap task
+  //wifiap.end();
+}
+
+void gpstest_to_sleep() {
+
+  led.setcolor(0, 0, 0, 0); // Black
+  // Suspend barometer task
+  // Suspend humidity task
+  // Suspend particulate task
+  // Suspend gps task
+  barometer.end();
+  humidity.end();
+  particulate.end();
+  //gps.end();
+}
+
+void gpstest_to_collect() {
+
+  led.setcolor(0, 0, 63, 0); // Green
+  barometer.begin();
+  humidity.begin();
+  particulate.begin();
+  // Resume barometer task
+  // Resume humidity task
+  // Resume particulate task
+}
+
+void collect_to_gpstest() {
+}
+
+void wifitest_to_sleep() {
+  led.setcolor(0, 0, 0, 0); // Black
+
+  // Suspend wificlient task
+  //wificlient.end();
+}
+
+void wifitest_to_upload() {
+  led.setcolor(0, 0, 0, 63); // Blue
+}
+
+void upload_to_wifitest() {
+  led.setcolor(0, 63, 0, 63); // Purple
+}
+
+void setup() {
+  state = START;
+
+  Serial.begin(SERIAL_BAUD);
+  Serial.println("Setup started.");
+  Serial.println("Serial communication initialized...");
+
+  led.begin();
+  Serial.println("LED initialized...");
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+
+  switch(state) {
+    case START:     start_state(); break;
+    case SLEEP:     sleep_state(); break;
+    case CONFIG:    config_state(); break;
+    case GPSTEST:   gpstest_state(); break;
+    case COLLECT:   collect_state(); break;
+    case WIFITEST:  wifitest_state(); break;
+    case UPLOAD:    upload_state(); break;
+    default:        start_state(); break;
+  }
+
+  if (state != next_state) {
+    Serial.print("Transition from "); Serial.print(states[state]);
+    Serial.print(" to "); Serial.println(states[next_state]);
+
+    switch(state) {
+
+      case START:
+        switch(next_state) {
+          case SLEEP:     start_to_sleep(); break;
+          default:        next_state = RESET;
+        }; Serial.println("During case."); delay(100); break;
+
+      case SLEEP:
+        switch(next_state) {
+          case CONFIG:    sleep_to_config(); break;
+          case GPSTEST:   sleep_to_gpstest(); break;
+          case WIFITEST:  sleep_to_wifitest(); break;
+          default:        next_state = RESET;
+        }; break;
+
+      case CONFIG:
+        switch(next_state) {
+          case SLEEP:     config_to_sleep(); break;
+          default:        next_state = RESET;
+        }; break;
+
+      case GPSTEST:
+        switch(next_state) {
+          case SLEEP:     gpstest_to_sleep(); break;
+          case COLLECT:   gpstest_to_collect(); break;
+          default:        next_state = RESET;
+        }; break;
+
+      case COLLECT:
+        switch(next_state) {
+          case GPSTEST:   collect_to_gpstest(); break;
+          default:        next_state = RESET;
+        }; break;
+
+      case WIFITEST:
+        switch(next_state) {
+          case SLEEP:     wifitest_to_sleep(); break;
+          case UPLOAD:    wifitest_to_upload(); break;
+          default:        next_state = RESET;
+        }; break;
+
+      case UPLOAD:
+        switch(next_state) {
+          case WIFITEST:  upload_to_wifitest(); break;
+          default:        next_state = RESET;
+        }; break;
 
     }
 
-  } else if (state = UPLOAD) {
+    Serial.println("End of transition."); 
 
-    // Upload action finishes successfully or times out
-    // Create JSON of X records
-    // Send to server
-    // Empty datastore
+    prev_state = state;
+    state = next_state;
 
-    state = WIFI_TEST;
-    led.setcolor(0, 63, 0, 63); // Purple
-
+    // FIXME: Set the led here based on state
   }
 
   schedule->tick();
+
 }
+
+// vim: syntax=c
