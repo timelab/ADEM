781ffa4972a838b97a68a312a08c20a6783cbfdc
kavers1 <koen.vxxxxxxx@scarlet.be>
2016-04-21 22:21:01 +0200

    the typical pointer issue :-(

--- libraries/TickerSchedlr/src/TickerSchedlr.cpp
+++ libraries/TickerSchedlr/src/TickerSchedlr.cpp
@@ -86,7 +86,7 @@ void TickerSchedlr::tick(){
 		if (task){
 			__LOG(" "); __LOG(task->name); __LOG("->"); __LOG(task->tasktype);
 			/* check if the task has been scheduled to run now or in the past */
-			if (task && __TASK_IS_REGULAR(*task) && task._enabled) {
+			if (task && __TASK_IS_REGULAR(*task) && task->_enabled) {
 				__LOG(" is a regular task");
 				/* task has been scheduled to execute */
 				if (task->getNextTime() <= _ticktime){
@@ -121,7 +121,7 @@ void TickerSchedlr::tick(){
 	while (_nextSchedule > updateTickTime() && i < _maxTsk) {
 		TickerTask * task = tasks[_idlePtr++];
 		// execute idle tasks 
-		if (task && __TASK_IS_IDLE(*task) && task._enabled){
+		if (task && __TASK_IS_IDLE(*task) && task->_enabled){
 			__LOGLN(" IDLE tasks");
 			yield();
 			task->exec();
