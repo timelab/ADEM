f03977271a46991be70e764c48543503e9bf8aa9
Dag Wieers <dag@wieers.com>
2016-05-12 00:06:23 +0200

    Fix Travis build error

--- libraries/StoreAndForward/store_and_forward.cpp
+++ libraries/StoreAndForward/store_and_forward.cpp
@@ -16,7 +16,7 @@ License along with this library; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-#include "store_And_Forward.h"
+#include "store_and_forward.h"
 #include <ctype.h>
 
 storeAndForwardBuf::storeAndForwardBuf(size_t size) :
