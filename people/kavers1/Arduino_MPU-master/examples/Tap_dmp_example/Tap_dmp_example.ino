eba40979520cceaba7d16e21774fe36b1815006f
kavers1 <koen.verstinge@scarlet.be>
2017-01-31 21:25:21 +0100

    tap update

--- people/kavers1/Arduino_MPU-master/examples/Tap_dmp_example/Tap_dmp_example.ino
+++ people/kavers1/Arduino_MPU-master/examples/Tap_dmp_example/Tap_dmp_example.ino
@@ -3,9 +3,11 @@
 #include <arduino_mpu.h>
 #include <inv_mpu.h>
 #include <inv_mpu_dmp_motion_driver.h>
+#include <Wire.h>
+#include <I2Cdev.h>
 
-#define PL(x) Serial.println(x)
-#define P(x) Serial.print(x)
+#define __LOGLN(x) Serial.println(x)
+#define __LOG(x) Serial.print(x)
 
 #define DEFAULT_MPU_HZ 4
 
@@ -19,9 +21,9 @@ short state = 0;
 bool standstill = false;
 
 void tap_cb(unsigned char i, unsigned char k) {
-    P("MPU: ");PL("==============================================================");
-    P("MPU: ");P(">>>>>>>>>>>>>tap detected ");P(i);P("........."); P(k); PL(" keep on going<<<<<<<<<");
-    P("MPU: ");PL("==============================================================");
+    __LOG("MPU: ");__LOGLN("==============================================================");
+    __LOG("MPU: ");__LOG(">>>>>>>>>>>>>tap detected ");__LOG(i);__LOG("........."); __LOG(k); __LOGLN(" keep on going<<<<<<<<<");
+    __LOG("MPU: ");__LOGLN("==============================================================");
     state = 3;
     standstill = false;
 }
@@ -40,20 +42,23 @@ unsigned short gyro_rate, gyro_fsr;
 unsigned char accel_fsr;
 float gyro_sens;
 unsigned short accel_sens;
-void setup() {
+int nDevices = 0;
+int error;
+
+void setu__LOG() {
   Serial.begin(115200);
   while(!Serial);
   struct int_param_s params;
   params.pin = 3;
   params.cb = interrupt;
-  PL(mpu_init(&params));
-  PL(mpu_set_sensors(INV_XYZ_GYRO | INV_XYZ_ACCEL));
-  PL(mpu_configure_fifo(INV_XYZ_GYRO | INV_XYZ_ACCEL));
-  PL(mpu_set_sample_rate(DEFAULT_MPU_HZ));
+  __LOGLN(mpu_init(&params));
+  __LOGLN(mpu_set_sensors(INV_XYZ_GYRO | INV_XYZ_ACCEL));
+  __LOGLN(mpu_configure_fifo(INV_XYZ_GYRO | INV_XYZ_ACCEL));
+  __LOGLN(mpu_set_sample_rate(DEFAULT_MPU_HZ));
   
-  mpu_get_sample_rate(&gyro_rate); P("FIFOrate: ");P(gyro_rate);PL("Hz");
-  mpu_get_gyro_fsr(&gyro_fsr); P("Gyro FSR: +/- ");P(gyro_fsr);PL("DPS");
-  mpu_get_accel_fsr(&accel_fsr); P("Accel FSR: +/- ");P(accel_fsr);PL("G");
+  mpu_get_sample_rate(&gyro_rate); __LOG("FIFOrate: ");__LOG(gyro_rate);__LOGLN("Hz");
+  mpu_get_gyro_fsr(&gyro_fsr); __LOG("Gyro FSR: +/- ");__LOG(gyro_fsr);__LOGLN("DPS");
+  mpu_get_accel_fsr(&accel_fsr); __LOG("Accel FSR: +/- ");__LOG(accel_fsr);__LOGLN("G");
 
   dmp_register_tap_cb(tap_cb);
 
@@ -62,21 +67,58 @@ void setup() {
   dmp_set_tap_thresh(2,20);
   dmp_set_tap_thresh(3,30);
 
-  PL(dmp_load_motion_driver_firmware());
-  PL(dmp_enable_feature(DMP_FEATURE_TAP | DMP_FEATURE_SEND_RAW_ACCEL | DMP_FEATURE_SEND_CAL_GYRO | DMP_FEATURE_GYRO_CAL));
+  __LOGLN(dmp_load_motion_driver_firmware());
+  __LOGLN(dmp_enable_feature(DMP_FEATURE_TAP | DMP_FEATURE_SEND_RAW_ACCEL | DMP_FEATURE_SEND_CAL_GYRO | DMP_FEATURE_GYRO_CAL));
   dmp_set_tap_thresh(TAP_XYZ, 250);
   dmp_set_tap_axes(TAP_XYZ);
   dmp_set_tap_count(2);
   dmp_set_tap_time(100);
   dmp_set_tap_time_multi(500);
 
-  PL(dmp_set_fifo_rate(DEFAULT_MPU_HZ));
-  PL(mpu_set_dmp_state(1));
+  __LOGLN(dmp_set_fifo_rate(DEFAULT_MPU_HZ));
+  __LOGLN(mpu_set_dmp_state(1));
 
   mpu_get_gyro_sens(&gyro_sens);
   mpu_get_accel_sens(&accel_sens);
 
   pixels.begin(); // This initializes the NeoPixel library.
+
+  if (nDevices == 0)
+  {
+  Wire.begin();
+  for(int address = 1; address < 127; address++ ) 
+  {
+    // The i2c_scanner uses the return value of
+    // the Write.endTransmisstion to see if
+    // a device did acknowledge to the address.
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+
+    if (error == 0)
+    {
+      Serial.print("I2C - 0x");
+      if (address<16) 
+        Serial.print("0");
+      Serial.println(address,HEX);
+      nDevices++;
+    }
+    else if (error==4) 
+    {
+      Serial.print("Unknown error at 0x");
+      if (address<16) 
+        Serial.print("0");
+      Serial.println(address,HEX);
+    }    
+  }
+  if (nDevices == 0) 
+  {
+    Serial.println("No I2C devices found\n");
+  }
+  else
+  {
+    Serial.println("done\n");
+  }
+  }
 
 }
 
@@ -91,8 +133,8 @@ short threshold = 150;
 unsigned long sensor_timestamp;
 float anorm;
 
-
-void loop() {
+void loo__LOG() {
+  
   if(dataReady){
 //    mpu_get_int_status(&intstatus);
     Serial.print("interupt status : ");Serial.println(intstatus);
@@ -103,7 +145,15 @@ void loop() {
     if(sensors & INV_XYZ_ACCEL)
       Serial.print("Acce: ");Serial.print(accel[0]);Serial.print(" ");Serial.print(accel[1]);Serial.print(" ");Serial.println(accel[2]);
     Serial.print("Delta:");Serial.print(accel[0]-accl[0]);Serial.print(" ");Serial.print(accel[1]-accl[1]);Serial.print(" ");Serial.println(accel[2]-accl[2]);
-      if (state != 3)
+    uint32_t lon = 0;
+    uint32_t lat = 0;
+    uint32_t time= 0;
+    long tmo = 0;
+    I2Cdev::readBytes(32,19,4,(uint8_t *)&lon);
+    I2Cdev::readBytes(32,15,4,(uint8_t *)&lat);
+    I2Cdev::readBytes(32,11,4,(uint8_t *)&time);
+    Serial.print(lat);Serial.print("  ");Serial.print(lon);Serial.print(" ");Serial.print(time);
+    if (state != 3)
       {
         if ((abs(accel[0]-accl[0]) < threshold) && (abs(accel[0]-accl[0]) < threshold) & (abs(accel[0]-accl[0]) < threshold) )
         {  
@@ -119,6 +169,38 @@ void loop() {
               Serial.println("!!!!!!!!!!!!!!!! we came to a halt !!!!!!!!!!!!!!!!!");
               dmp_enable_feature(DMP_FEATURE_TAP);
               state = 2;
+              for(int address = 1; address < 127; address++ ) 
+  {
+    // The i2c_scanner uses the return value of
+    // the Write.endTransmisstion to see if
+    // a device did acknowledge to the address.
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+
+    if (error == 0)
+    {
+      Serial.print("I2C - 0x");
+      if (address<16) 
+        Serial.print("0");
+      Serial.println(address,HEX);
+      nDevices++;
+    }
+    else if (error==4) 
+    {
+      Serial.print("Unknown error at 0x");
+      if (address<16) 
+        Serial.print("0");
+      Serial.println(address,HEX);
+    }    
+  }
+  if (nDevices == 0) 
+  {
+    Serial.println("No I2C devices found\n");
+  }
+  else
+  {
+    Serial.println("done\n");
+  }
             }
         }
         else
@@ -132,7 +214,7 @@ void loop() {
       accl[1] = accel[1];
       accl[2] = accel[2];
       Serial.print(" ");Serial.println(state);
-      PL();
+      __LOGLN();
       }
       for(int i=0;i<1;i++){
         switch (state){
